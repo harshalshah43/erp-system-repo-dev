4585f3f155293bcfa263b8357944bf29f4302e17
Kamakshi Mehta <50184655+KamakshiMehta@users.noreply.github.com>
2026-09-25 15:39:28 +0530

    Update home.py

--- kamakshiClass/api/home.py
+++ kamakshiClass/api/home.py
@@ -119,7 +119,7 @@ def suppliers():
                 <h1>Suppliers</h1>
                 <table border="1">
                     <th>Supplier ID</th>
-                    <th>SupplierName</th>
+                    <th>Supplier Name</th>
                     <th>Contact Email</th>
         """
         for row in rows:
